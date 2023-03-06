ade864e74f12fb4301f8bb920005f092afe4237e
Fábio Cabaceira <fabiocabaceira@gmail.com>
2023-03-06 15:04:34 +0000

    Navbar

--- ui/src/app/page.js
+++ ui/src/app/page.js
@@ -1,10 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "src/Nav/Navbar";
 
+
 export default function Home() {
   return (
-  <div> 
-    <Navbar />
-    </div>
-  );
+  "-"
+  )
 }
